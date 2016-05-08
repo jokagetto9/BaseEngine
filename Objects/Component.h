d9199ebe72fec5375ed78ca3b3caac05cab932d6
jokagetto9 <jokagetto9@gmail.com>
2016-05-07 19:43:44 -0600

    Component Prep

---
+++ Objects/Component.h
@@ -0,0 +1,16 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#include "../lib.h"
+
+//********************************* CONSTANTS *********************************
+
+//********************************* INITIALIZATION *********************************
+
+
+class Compoment  {
+	public:
+			
+};
+
+#endif
