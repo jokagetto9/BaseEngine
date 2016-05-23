7ba2bb0d7cd694c34b5894554a37630b57573e27
jokagetto9 <jokagetto9@gmail.com>
2016-05-23 14:32:47 -0600

    Removing Stack

---
+++ Entities/EntityDictionary.h
@@ -0,0 +1,24 @@
+#ifndef ACTORPROFILE_H
+#define ACTORPROFILE_H
+
+#include "../BaseEngine/Components/Animation.h"
+//********************************* CONSTANTS *********************************
+
+
+//********************************* INITIALIZATION *********************************
+
+class ActorProfile {
+	public: 
+		ID walkTex;
+		Animation walkAnim;
+		//Animation dieAnim;
+		
+		string name;
+		
+		//stats;
+		 
+		//max health
+		//max speed
+
+};
+#endif
