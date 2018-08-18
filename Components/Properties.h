80e0cf7257f929367fda3d51e5e5150d55c95475
jokagetto9 <jokagetto9@gmail.com>
2018-08-18 13:22:55 -0600

    properties

--- Components/Properties.h
+++ Components/Properties.h
@@ -15,4 +15,10 @@ public:
 
 };
 
+/*
+properties determine which checks are made in each system
+each entity has a current, temp and default properties set
+the requirements for transitions are set within the character's state transition functions
+
+*/
 #endif
